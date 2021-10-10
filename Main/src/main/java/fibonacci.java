d84f02b2671e537be1a313e6362275c509412f0e
qviloo <qviloo@gmail.com>
2021-10-10 17:09:18 +0400

    Updated algorithm (props to some indian guy) quicker added some input checking

--- Main/src/main/java/fibonacci.java
+++ Main/src/main/java/fibonacci.java
@@ -1,17 +1,32 @@
+import java.util.InputMismatchException;
 import  java.util.Scanner;
 
 public class fibonacci {
 
-    public static int fib(int i) {
+    public static long fib(long i) {
         if (i <= 1) return i;
-        return fib(i - 1) + fib(i - 2);
+        long prevFib = 0, currFib = 1;
+        for (long k = 0; k < i-1; k++){
+            long newFib = prevFib + currFib;
+            prevFib=currFib;
+            currFib=newFib;
+        }
+        return currFib;
     }
 
     public static void main(String[] args) {
-        int i;
-        System.out.println("Give me index and i'll give you fibonacci number:");
+        long  i=69;
+
+
         Scanner sc = new Scanner(System.in);
-        i = sc.nextInt();
+        try
+        {
+            System.out.println("Give me index and i'll give you fibonacci number:");
+            i = sc.nextInt();
+        }
+        catch (InputMismatchException exception) {
+            System.out.println("hey man, just give me a number");
+        }
 
         System.out.println("soo, anyway Fibonacci number by index "+i+" is: "+ fib(i));
     }
