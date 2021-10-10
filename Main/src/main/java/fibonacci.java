7452041310822bbf906ad98f06e0c39d3c3c8f62
qviloo <qviloo@gmail.com>
2021-10-10 15:48:11 +0400

    first version of fibonacci index finder

---
+++ Main/src/main/java/fibonacci.java
@@ -0,0 +1,20 @@
+import  java.util.Scanner;
+
+public class fibonacci {
+
+    public static int fib(int i) {
+        if (i <= 1) return i;
+        return fib(i - 1) + fib(i - 2);
+    }
+
+    public static void main(String[] args) {
+        int i;
+        System.out.println("Give me index and i'll give you fibonacci number:");
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+
+        System.out.println(fib(i));
+    }
+
+
+}
